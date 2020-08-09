e82b43feff7ec0b560ccb3e6d8e20770976ef8a4
HAYAMA_Kaoru <kaoru.hayama@alfatech.jp>
2020-08-10 00:10:12 +0900

    Split Convert method to some class-methods

--- main.go
+++ main.go
@@ -10,43 +10,48 @@ import (
 	"strings"
 )
 
-func Convert(files []string, zipFd io.Writer) error {
-	data := map[string][]string{}
-	for _, fname := range files {
-		fd, err := os.Open(fname)
+type Storage struct {
+	data map[string][]string
+}
+
+func New() *Storage {
+	return &Storage{
+		data: map[string][]string{},
+	}
+}
+
+func (storage *Storage) Len() int {
+	return len(storage.data)
+}
+
+func (storage *Storage) Add(r io.Reader, warn io.Writer) error {
+	reader := csv.NewReader(r)
+	reader.Comma = '\t'
+	reader.FieldsPerRecord = -1 // field number check off
+	for {
+		record, err := reader.Read()
 		if err != nil {
-			return err
-		}
-		reader := csv.NewReader(fd)
-		reader.Comma = '\t'
-		reader.FieldsPerRecord = -1 // field number check off
-		for {
-			record, err := reader.Read()
-			if err != nil {
-				if err != io.EOF {
-					fd.Close()
-					return fmt.Errorf("%s: %w", fname, err)
-				}
-				break
+			if err != io.EOF {
+				return err
 			}
-			if len(record) < 2 {
-				fmt.Fprintf(os.Stderr, "%s: too few fields in line: '%s'\n",
-					fname,
-					strings.Join(record, "\t"))
-				continue
-			}
-			key := record[1]
-			val := strings.Join(record[2:], "\t")
-			data[key] = append(data[key], val)
+			break
 		}
-		fd.Close()
+		if len(record) < 2 {
+			fmt.Fprintf(warn, "too few fields in line: '%s'\n",
+				strings.Join(record, "\t"))
+			continue
+		}
+		key := record[1]
+		val := strings.Join(record[2:], "\t")
+		storage.data[key] = append(storage.data[key], val)
 	}
-	if len(data) <= 0 {
-		return errors.New("no data")
-	}
-	zipWriter := zip.NewWriter(zipFd)
+	return nil
+}
+
+func (storage *Storage) DumpZip(w io.Writer) error {
+	zipWriter := zip.NewWriter(w)
 	defer zipWriter.Close()
-	for date, lines := range data {
+	for date, lines := range storage.data {
 		tsvName := strings.Replace(date, "/", "", -1) + ".tsv"
 		fd, err := zipWriter.Create(tsvName)
 		if err != nil {
@@ -60,5 +65,25 @@ func Convert(files []string, zipFd io.Writer) error {
 		}
 	}
 	zipWriter.Flush()
+	return nil
+}
+
+func Convert(files []string, zipFd io.Writer) error {
+	storage := New()
+	for _, fname := range files {
+		fd, err := os.Open(fname)
+		if err != nil {
+			return err
+		}
+		err = storage.Add(fd, os.Stderr)
+		fd.Close()
+		if err != nil {
+			return fmt.Errorf("%s: %w", fname, err)
+		}
+	}
+	if storage.Len() <= 0 {
+		return errors.New("no data")
+	}
+	storage.DumpZip(zipFd)
 	return nil
 }
